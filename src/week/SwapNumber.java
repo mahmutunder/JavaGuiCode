5e05754c6ad16b6039d424b97b9bcbc2e3508e6d
mahmutunder <mahmutunder@gmail.com>
2023-01-04 22:09:03 -0700

    method first swap

--- src/week/SwapNumber.java
+++ src/week/SwapNumber.java
@@ -1,4 +1,19 @@
 package week;
 
 public class SwapNumber {
+    public static void main(String[] args) {
+        //TODO: Swap 2 variable values without using third variable
+        //:TODO Method one
+        int x=20;
+        int y=10;
+
+         y=x+y;// 30 y
+         x=y-x; // 20 x
+         y=y-x;
+
+        System.out.println("x : " +x);
+        System.out.println("y : " +y);
+
+
+    }
 }
