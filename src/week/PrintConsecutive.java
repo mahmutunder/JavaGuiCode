7cb6a9ea4be79bdfdaeaea73f0e5eccd2d258b4d
mahmutunder <mahmutunder@gmail.com>
2023-01-04 22:42:33 -0700

    add method

--- src/week/PrintConsecutive.java
+++ src/week/PrintConsecutive.java
@@ -3,5 +3,33 @@ package week;
 public class PrintConsecutive {
     public static void main(String[] args) {
 
+
+        printConsecutiveNumbers(20);
+    }
+
+    public static void printConsecutiveNumbers(int n) {
+
+        int i = 1;
+        while (i <= n) {
+
+            if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0) {
+                System.out.println("CodilityTestCoders");
+            } else if (i % 2 == 0 && i % 5 == 0) {
+                System.out.println("CodilityCoders");
+            } else if (i % 2 == 0 && i % 3 == 0) {
+                System.out.println("CodilityTest");
+            } else if (i % 5 == 0) {
+                System.out.println("Coders");
+            } else if (i % 3 == 0) {
+                System.out.println("Test");
+            } else if (i % 2 == 0) {
+                System.out.println("Codility");
+            } else {
+                System.out.println(i);
+            }
+
+            i++;
+        }
+
     }
 }
