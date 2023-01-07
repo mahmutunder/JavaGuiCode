9020e547625078a1cf923a4aa09f90843a05dda9
mahmutunder <mahmutunder@gmail.com>
2023-01-06 23:40:54 -0700

    build OpenNewPage

--- src/zuhalTest/OpenNewFrame.java
+++ src/zuhalTest/OpenNewFrame.java
@@ -1,4 +1,22 @@
 package zuhalTest;
 
+import javax.swing.*;
+
 public class OpenNewFrame {
+    JFrame frame = new JFrame();
+    JLabel label= new JLabel("Hello World");
+
+    OpenNewFrame(){
+
+        frame.setDefaultCloseOperation(3);
+        frame.setSize(420,420);
+        frame.setLayout(null);
+
+        frame.setLocation(600,400);
+
+
+        frame.add(label);
+        frame.setVisible(true);
+
+    }
 }
