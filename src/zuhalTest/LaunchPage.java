e5ee4cc26edc7f48f11d3cd51b059a93566aed49
mahmutunder <mahmutunder@gmail.com>
2023-01-06 23:35:31 -0700

    LaunchPaged build

--- src/zuhalTest/LaunchPage.java
+++ src/zuhalTest/LaunchPage.java
@@ -1,5 +1,31 @@
 package zuhalTest;
 
-public class LaunchPage {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class LaunchPage  implements ActionListener {
+    JFrame frame = new JFrame();
+    JButton btn= new JButton("New Page");
+    LaunchPage(){
+
+        frame.setDefaultCloseOperation(3);
+        frame.setSize(420,420);
+        frame.setLayout(null);
+
+        btn.setBounds(100,160,200,40);
+        btn.setFocusable(false);
+        btn.addActionListener(this);
+
+        frame.add(btn);
+        frame.setVisible(true);
+
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
