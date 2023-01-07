dc335fe6c9f2249332cde5fc14d4577cc2c84766
mahmutunder <mahmutunder@gmail.com>
2023-01-07 00:27:59 -0700

    updated

--- src/Hack/ListOption.java
+++ src/Hack/ListOption.java
@@ -1,11 +1,17 @@
 package Hack;
 
+import javax.swing.*;
+
 public class ListOption {
     public static void main(String[] args) {
 
         String[] options={"Java","C#","Javascript","C++","Python"};
 
+        int answer= JOptionPane.showOptionDialog(null,
+                "What is best Language to create app?","Best Language",
+               JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE,null,options,0);
 
+        JOptionPane.showMessageDialog(null,("Maybe " + options[answer]),"Answer",JOptionPane.INFORMATION_MESSAGE);
 
 
     }
