39662d750455f0b9a679bab9696f6c9f427cbb47
mahmutunder <mahmutunder@gmail.com>
2023-01-09 00:02:56 -0700

    build combobox class

--- src/Hack/ComboBox.java
+++ src/Hack/ComboBox.java
@@ -1,8 +1,44 @@
 package Hack;
 
-public class ComboBox {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ComboBox extends JFrame implements ActionListener {
+
+    private JComboBox comboBox;
+
+    public ComboBox(){
+
+        setTitle("ComboBox");
+        setDefaultCloseOperation(3);
+        setLayout(new FlowLayout());
+
+        // takes array of objects
+        String[] animals={"Cat","Dog","Horse","Bird"};
+
+        comboBox= new JComboBox(animals);
+        comboBox.addActionListener(this);
+
+
+        add(comboBox);
+        setLocation(500,350);
+        setSize(400,250);
+        setVisible(true);
+    }
     public static void main(String[] args) {
 
 
+        new ComboBox();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource()==comboBox){
+            System.out.println(comboBox.getSelectedIndex());
+            System.out.println(comboBox.getItemCount());
+        }
     }
 }
